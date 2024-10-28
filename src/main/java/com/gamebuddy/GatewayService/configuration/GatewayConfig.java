60f5ee83ba3fd374c8ee2da79a8b9fe73493e621
Görkem <gorkem_yelken@hotmail.com>
2024-10-28 17:52:31 +0300

    Updated Routes

--- src/main/java/com/gamebuddy/GatewayService/configuration/GatewayConfig.java
+++ src/main/java/com/gamebuddy/GatewayService/configuration/GatewayConfig.java
@@ -12,13 +12,13 @@ public class GatewayConfig {
     public RouteLocator customRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service", r -> r.path("/api/v1/users/**")
-                        .uri("https://gamebuddy-user-service-04b8e7746067.herokuapp.com/")) // Kullanıcı servisi
+                        .uri("https://gamebuddy-user-service-04b8e7746067.herokuapp.com")) // Kullanıcı servisi
                 .route("game-service", r -> r.path("/api/v1/games/**")
-                        .uri("https://gamebuddy-game-service-1355a6fbfb17.herokuapp.com/")) // Oyun servisi
+                        .uri("https://gamebuddy-game-service-1355a6fbfb17.herokuapp.com")) // Oyun servisi
                 .route("matchmaking-service", r -> r.path("/api/v1/matchmaking/**")
-                        .uri("https://gamebuddy-matchmaking-service-c814497a7748.herokuapp.com/")) // Eşleştirme servisi
+                        .uri("https://gamebuddy-matchmaking-service-c814497a7748.herokuapp.com")) // Eşleştirme servisi
                 .route("auth-service", r -> r.path("/api/v1/auth/**")
-                        .uri("https://gamebuddy-auth-service-b40a307cb66b.herokuapp.com/")) // Eşleştirme servisi
+                        .uri("https://gamebuddy-auth-service-b40a307cb66b.herokuapp.com")) // Eşleştirme servisi
                 .build();
     }
 }
